efedf6c6460bb34e28adcc0f5881d6091f562f49
NataliiaOsintseva <nataly.osya@gmail.com>
2016-07-18 12:34:12 +0300

    Add files via upload

---
+++ src/Travel/Services/RealMailService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Services
+{
+    public class RealMailService : IMailService
+    {
+        public bool SendMail(string to, string from, string subject, string body)
+        {
+            Console.WriteLine($"Sending mail: To: {to}, Subject: {subject}");
+            return true;
+        }
+    }
+}
