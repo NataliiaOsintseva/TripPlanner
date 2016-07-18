f967d027cdfea903991a4289f3a3e5e219a630b8
NataliiaOsintseva <nataly.osya@gmail.com>
2016-07-18 12:20:10 +0300

    Add files via upload

---
+++ src/Travel/wwwroot/js/app-trips.js
@@ -0,0 +1,27 @@
+// app-trips.js
+
+(function () {
+
+    "use strict";
+
+    // Getting existing module
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+    .config(function ($routeProvider) {
+
+        $routeProvider.when("/", {
+            controller: "tripsController",
+            controllerAs: "vm",
+            templateUrl: "/views/tripsView.html"
+        });
+
+        $routeProvider.when("/editor/:tripName", {
+            controller: "tripEditorController",
+            controllerAs: "vm",
+            templateUrl: "/views/tripEditorView.html"
+        });
+
+        $routeProvider.otherwise({ redirectTo: "/" });
+
+    });
+
+})();
